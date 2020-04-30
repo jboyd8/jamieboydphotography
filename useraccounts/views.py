463ce325a804266afe8e7ecb5d39b5c7a344e609
jboyd8 <jamieboyd8@yahoo.com>
2020-04-30 21:36:31 +0100

    feat: added login functionality

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import auth
+from django.contrib.auth import logout
+from .forms import LoginForm
 
 
 def register(request):
@@ -9,10 +13,30 @@ def register(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        pass
+    if request.user.is_authenticated:
+        messages.error(request, 'You are already logged in')
+        return redirect('index')
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        if form.is_valid():
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'You are now logged in')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid credentials')
+                return redirect('login')
     else:
-        return render(request, 'useraccounts/login.html')
+        login_form = LoginForm()
+
+    context = {
+        'login': 'active',
+        'form': login_form
+    }
+    return render(request, 'useraccounts/login.html', context)
 
 
 def logout(request):
