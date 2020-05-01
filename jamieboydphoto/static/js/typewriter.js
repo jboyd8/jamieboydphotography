6104edfb8af14e205f828fd2babf34fc59715786
jboyd8 <jamieboyd8@yahoo.com>
2020-05-01 11:02:25 +0100

    docs: added comments

--- jamieboydphoto/static/js/typewriter.js
+++ jamieboydphoto/static/js/typewriter.js
@@ -1,9 +1,12 @@
+// select the div with typewriter app id
 let app = document.getElementById('typewriter-app');
 
+// Create a new instance of the Typewirter app and keep it looping
 let typewriter = new Typewriter(app, {
     loop:true
 });
 
+// Add contect to the typewriter and attributes
 typewriter.typeString('Jamie Boyd Photography')
     .pauseFor(3000)
     .deleteAll()
