d98e73aff7418f3018887373b05a5b98d09e466c
jboyd8 <jamieboyd8@yahoo.com>
2020-05-01 15:34:16 +0100

    feat: completed blogs view

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Blogs
 
 
 def blogs(request):
     """FILL IN THIS COMMENT"""
 
+    display_blogs = Blogs.objects.order_by('-date_added').filter(is_published=True)
+
+    paginator = Paginator(display_blogs, 3)
+    page = request.GET.get('page')
+    paged_blogs = paginator.get_page(page)
+
     context = {
-        'blogs': 'active'
+        'blogs': 'active',
+        'blog_posts': paged_blogs
     }
     return render(request, 'blog/blogs.html', context)
 
 
-def blog(request):
+def blog(request, blog_id):
     pass
 
