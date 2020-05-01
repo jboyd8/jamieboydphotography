2f28ba5030746e99b32ac786d9060d34fa6cf0fe
jboyd8 <jamieboyd8@yahoo.com>
2020-05-01 10:13:49 +0100

    feat: added register form

--- useraccounts/forms.py
+++ useraccounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import ValidationError
+from django.contrib.auth.models import User
 
 
 class LoginForm (forms.Form):
@@ -19,3 +22,83 @@ class LoginForm (forms.Form):
             'placeholder': 'Password'
         })
     )
+
+
+class RegForm(UserCreationForm):
+
+    first_name = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'First Name'
+        })
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Last Name'
+        })
+    )
+
+    username = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+    )
+
+    email = forms.EmailField(
+        required=True,
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+    )
+
+    password1 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError('Email address already in use')
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError('Please confirm your password')
+
+        if password1 != password2:
+            raise ValidationError('Passwords do not match')
+
+        return password2
