9799c5464b8f3e98b54f795220a16254529fa99d
jboyd8 <jamieboyd8@yahoo.com>
2020-05-01 10:14:32 +0100

    fix: completed register function

--- useraccounts/views.py
+++ useraccounts/views.py
@@ -2,14 +2,39 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import auth
 from django.contrib.auth import logout
-from .forms import LoginForm
+from .forms import LoginForm, RegForm
 
 
 def register(request):
-    if request.method == 'POST':
-        pass
+
+    if request.user.is_authenticated:
+        messages.error(request, 'You are already logged in')
+        return redirect('index')
+
+    elif request.method == 'POST':
+        form = RegForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password1']
+        if form.is_valid():
+            form.save()
+            user = auth.authenticate(username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                messages.success(request, 'You have been successfully registered. Welcome')
+                return redirect('index')
+
+            else:
+                messages.error(request, 'Unable to register your account at this time')
     else:
-        return render(request, 'useraccounts/register.html')
+        form = RegForm()
+
+    context = {
+        'register': 'active',
+        'form': form
+    }
+
+    return render(request, 'useraccounts/register.html', context)
 
 
 def login(request):
