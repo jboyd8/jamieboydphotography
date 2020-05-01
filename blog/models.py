42ab9efe5dbfaaa817ccd3e8bff54c0c9c76350d
jboyd8 <jamieboyd8@yahoo.com>
2020-05-01 13:20:05 +0100

    feat: created blogs model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+genres = [
+    'Landscapes',
+    'Portraits',
+    'Cityscapes',
+    'Technical',
+    'General'
+]
+
+
+class Blogs(models.Model):
+    """Create model for the blogs app"""
+
+    blog_title = models.CharField(max_length=200)
+    blog_body = models.TextField(blank=True)
+    genre = models.CharField(choices=genres)
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.blog_title
