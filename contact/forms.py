c34caab352c08b8259d8adae90a10ccb934ee1f4
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 09:45:19 +0100

    feat: created form to pass through to html template

---
+++ contact/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.Form):
+
+    class Meta:
+        model = Contact
+        fields = ['contact_title', 'contact_body', 'email']
+
+    contact_title = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Title'
+        })
+    )
+
+    contact_body = forms.CharField(
+        required=True,
+        label='',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Message'
+        })
+    )
+
+    email = forms.EmailField(
+        required=True,
+        label='',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+    )
+
