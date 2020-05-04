07a359833c700502e110ee7f00f4cbdb7b9e1c39
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 16:37:31 +0100

    feat: added gallery to installed apps

--- jamieboydphoto/settings.py
+++ jamieboydphoto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'useraccounts.apps.UseraccountsConfig',
     'blog.apps.BlogConfig',
     'contact.apps.ContactConfig',
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
