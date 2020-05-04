2ffac5788e557854dcdf4c28c2c1980be4a7732f
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 19:54:13 +0100

    feat: created gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Gallery
 
 
 def gallery(request):
+    """Create gallery view. Show published images in order of most recently added."""
+
+    images = Gallery.objects.order_by('-date_added').filter(is_published=True)
 
     context = {
         'gallery': 'active',
+        'images': images
     }
 
     return render(request, 'gallery/gallery.html', context)
