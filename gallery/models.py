7eca27db4caf64ef20a9473b9da7fad47e507844
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 19:52:24 +0100

    feat: created gallery model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Gallery(models.Model):
+    """Create the model for the gallery app"""
+
+    image_title = models.CharField(max_length=200)
+    image = models.URLField(default='https://image.flaticon.com/icons/svg/38/38645.svg')
+    image_description = models.TextField(blank=True)
+    is_published = models.BooleanField(default=False)
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.image_title
