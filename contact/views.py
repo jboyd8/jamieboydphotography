42025cb39a8631d9f08a3bf535c1cd1fce2e17c6
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 15:33:12 +0100

    feat: added the logic for the contact from to work

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+import os
 from .forms import ContactForm
+from.models import Contact
+
+ADMINS_EMAIL = os.environ.get('ADMINS_EMAIL')
 
 
 def contact(request):
 
-    form = ContactForm()
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+
+            form = Contact(
+                contact_title=request.POST['contact_title'],
+                contact_body=request.POST['contact_body'],
+                email=request.POST['email'],
+                query_user=request.user
+            )
+
+            form.save()
+
+        else:
+            form = Contact(
+                contact_title=request.POST['contact_title'],
+                contact_body=request.POST['contact_body'],
+                email=request.POST['email']
+            )
+
+            form.save()
+
+        send_mail(
+            'New enquiry from JBP',
+            'You have a new message. See admin panel for details.',
+            os.environ.get('SITE_EMAIL'),
+            [ADMINS_EMAIL],
+            fail_silently=False,
+        )
+
+        messages.success(request, 'Your enquiry has been submitted, I will get back to you shortly.')
 
     context = {
         'contact': 'active',
-        'form': form
+        'form': ContactForm
     }
 
     return render(request, 'contact/contact.html', context)
