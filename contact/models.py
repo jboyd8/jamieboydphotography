45fa78b289d2b900e70152fd29e2c24b01634a20
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 16:02:33 +0100

    docs: added docstring

--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 
 class Contact(models.Model):
+    """Create the contact model. create foreign key to the user table. This will allow us to see what user
+    made an enquiry if they are logged in."""
+
     contact_title = models.CharField(max_length=200)
     contact_body = models.TextField(blank=True)
     email = models.CharField(max_length=200)
