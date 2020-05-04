2297fbcc298e601aeb8048095be07e0cc4d1c244
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 16:42:33 +0100

    fix: changed to just one gallery url

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('landscapes', views.landscapes, name='landscapes'),
-    path('portraits', views.portraits, name='portraits'),
-    path('cityscapes', views.logout_cityscapes, name='cityscapes'),
+    path('gallery', views.gallery, name='gallery'),
 ]
