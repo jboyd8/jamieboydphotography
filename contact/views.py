e87c3b4fabd820f15918fd9596cbf2fd1d4cce7f
jboyd8 <jamieboyd8@yahoo.com>
2020-05-04 09:48:16 +0100

    feat: created basic view to render contact html

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def contact(request):
-    pass
+
+    form = ContactForm()
+
+    context = {
+        'contact': 'active',
+        'form': form
+    }
+
+    return render(request, 'contact/contact.html', context)
