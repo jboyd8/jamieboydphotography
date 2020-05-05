ca90e832cdd3b3ceaf5b11d5f1103b56f459fedd
jboyd8 <jamieboyd8@yahoo.com>
2020-05-05 19:30:43 +0100

    feat: added logic to view comments and created add comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from .models import Blogs
+from comments.models import BlogComment
+from comments.forms import BlogCommentForm
+from django.contrib import messages
 
 
 def blogs(request):
@@ -21,10 +24,31 @@ def blogs(request):
 
 
 def blog(request, blog_id):
+    """
+    Return the requested blog post and show its related comments. Pass through the comment form in order to add a
+    comment to the blog post.
+    """
     post = get_object_or_404(Blogs, pk=blog_id)
 
+    blog_comments = get_object_or_404(BlogComment.objects.order_by('-date_posted').filter(
+        is_published=True,
+        blog_id=blog_id))
+
+    if request.method == 'POST':
+        form = BlogComment(
+            comment_body=request.POST['comment_body'],
+            comment_user=request.user,
+            post=post
+        )
+
+        form.save()
+        messages.success(request, 'Comment successfully added')
+        return redirect('blog', blog_id)
+
     context = {
-        'post': post
+        'post': post,
+        'blog_comments': blog_comments,
+        'form': BlogCommentForm
     }
 
     return render(request, 'blog/blog.html', context)
