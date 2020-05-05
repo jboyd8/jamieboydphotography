00885435f4b6bcc5d2a7d010329b13851ea4faa9
jboyd8 <jamieboyd8@yahoo.com>
2020-05-05 16:46:24 +0100

    feat: created comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from blog.models import Blogs
 
-# Create your models here.
+
+class BlogComment(models.Model):
+    """
+    Creates a model for once instance of a blog comment.
+    """
+    comment_body = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    comment_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    related_blog = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.comment_body
