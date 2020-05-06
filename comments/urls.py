787535982aef7656cf3293ad2e6f815fb811c773
jboyd8 <jamieboyd8@yahoo.com>
2020-05-06 19:37:06 +0100

    fix: added /delete to the end of the delete url which was causing the edit url to run over and over

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('<int:comment_id>', views.edit_comment, name='edit_comment'),
-    path('<int:comment_id>', views.delete_comment, name='delete_comment'),
+    path('<int:comment_id>/delete', views.delete_comment, name='delete_comment'),
 ]
