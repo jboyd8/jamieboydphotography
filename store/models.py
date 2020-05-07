9a07f5540ae914b60be2e89aa2acf4d605e40812
jboyd8 <jamieboyd8@yahoo.com>
2020-05-07 10:29:46 +0100

    feat: added product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='Print')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.URLField(default='https://image.flaticon.com/icons/svg/38/38645.svg')
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
