e06bb0906feeb63862b5b62cf2462d989adc8316
jboyd8 <jamieboyd8@yahoo.com>
2020-05-07 17:29:56 +0100

    fix: changed id type passed into url to str

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('view_cart', views.view_cart, name='view_cart'),
     path('add/<int:id>', views.add_to_cart, name='add_to_cart'),
-    path('adjust/<int:id>', views.adjust_cart, name='adjust_cart'),
+    path('adjust/<str:id>', views.adjust_cart, name='adjust_cart'),
 ]
