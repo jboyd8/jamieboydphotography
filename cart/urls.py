dc215b1cb1f492e9fe9c66694150c58fd4cbc5df
jboyd8 <jamieboyd8@yahoo.com>
2020-05-07 14:20:54 +0100

    fix: removed blog from add and adjust urls

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('view_cart', views.view_cart, name='view_cart'),
-    path('add/<int:blog_id>', views.add_to_cart, name='add_to_cart'),
-    path('adjust/<int:blog_id>', views.adjust_cart, name='adjust_cart'),
+    path('add/<int:id>', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<int:id>', views.adjust_cart, name='adjust_cart'),
 ]
