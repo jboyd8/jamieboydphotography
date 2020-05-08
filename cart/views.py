4d7bd048524e5a02b4d2f43386602f4fed80f354
jboyd8 <jamieboyd8@yahoo.com>
2020-05-08 08:03:43 +0100

    feat: added user auth to add to cart view

--- cart/views.py
+++ cart/views.py
@@ -13,19 +13,24 @@ def add_to_cart(request, id):
     """
      Adds the amount of a selected product to the cart
     """
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
+    if request.user.is_authenticated:
 
-    if id in cart:
-        cart[id] = int(cart[id] + quantity)
-    else:
-        cart[id] = cart.get(id, quantity)
+        quantity = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
 
-    print(cart)
+        if id in cart:
+            cart[id] = int(cart[id] + quantity)
+        else:
+            cart[id] = cart.get(id, quantity)
 
-    request.session['cart'] = cart
-    messages.success(request, 'You have successfully added an item to your cart.')
-    return redirect(reverse('store'))
+        print(cart)
+
+        request.session['cart'] = cart
+        messages.success(request, 'You have successfully added an item to your cart.')
+        return redirect(reverse('store'))
+
+    messages.error(request, 'You have to be logged in to add an item to your cart.')
+    return redirect('login')
 
 
 def adjust_cart(request, id):
